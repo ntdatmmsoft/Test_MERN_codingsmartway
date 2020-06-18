66070925c6594a4bf6bdb29645c7926647673c17
ntDat <dat.nt2212@sinhvien.hoasen.edu.vn>
2020-06-18 16:52:54 +0700

    update front end v1 clip

---
+++ mern-todo-app/src/components/edit-todo.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TodosList extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to Edit todo Component!!</p>
+            </div>
+        )
+    }
+}
