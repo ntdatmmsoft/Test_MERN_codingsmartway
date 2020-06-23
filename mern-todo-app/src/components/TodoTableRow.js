25d80014d6bb003beb7af8a7d9b0a8f097b82817
ntDat <dat.nt2212@sinhvien.hoasen.edu.vn>
2020-06-23 09:30:12 +0700

    updated Delete Button

---
+++ mern-todo-app/src/components/TodoTableRow.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+
+export default class TodoTableRow extends Component {
+
+    constructor(props) {
+        super(props);
+        this.delRecord = this.delRecord.bind(this);
+    }
+
+    delRecord() {
+        axios.delete('http://localhost:4000/todos/delete/' + this.props.todo._id)
+            .then((res) => {
+                console.log('Successfully deleted!');
+                window.location.reload();
+            }).catch((error) => {
+                console.log(error)
+            })
+    }
+
+    render() {
+        return (
+            <tr>
+                <td>{this.props.todo.todo_description}</td>
+                <td>{this.props.todo.todo_responsible}</td>
+                <td>{this.props.todo.todo_priority}</td>
+                <td>
+                    <Link class="edit-link" to={"/edit/" + this.props.todo._id}>
+                        Edit
+                    </Link>
+                </td>
+                <td>
+                    <Button onClick={this.delRecord} size="sm" variant="danger">Delete</Button>
+                </td>
+            </tr>
+        );
+    }
+}
--- mern-todo-app/src/components/todos-list.component.js
+++ mern-todo-app/src/components/todos-list.component.js
@@ -1,41 +1,16 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
-import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+import TodoTableRow from './TodoTableRow';
 //create link url for delete
 
-
-
-const Todo = props => (
-    <tr>
-        <td>{props.todo.todo_description}</td>
-        <td>{props.todo.todo_responsible}</td>
-        <td>{props.todo.todo_priority}</td>
-        <td>
-            <Link to={"/edit/" + props.todo._id}>Edit</Link>
-        </td>
-        {/* <td>
-            <Button onClick={this.delRecord}>Delete</Button>
-        </td> */}
-    </tr>
-)
-
-export default class TodosList extends React.Component {
+export default class TodosList extends Component {
     constructor(props) {
         super(props);
         this.state = { todos: [] };
-        this.delRecord = this.delRecord.bind(this);
+
     }
 
-    delRecord = () => {
-        // axios.path('http://localhost:4000/todos/delete' + this.props.obj._id)
-        //     .then((res) => {
-        //         console.log('Successfully deleted!')
-        //     }).catch((error) => {
-        //         console.log(error)
-        //     })
-        console.log('Delete function');
-    }
     componentDidMount() {
         axios.get('http://localhost:4000/todos/')
             .then(response => {
@@ -46,33 +21,9 @@ export default class TodosList extends React.Component {
             })
     }
 
-
-
     todoList = () => {
-        this.state.todos.map(function (currentTodo, i) {
-            const {
-                _id,
-                todo_description,
-                todo_responsible,
-                todo_priority,
-                todo_completed
-            } = currentTodo;
-            return <Todo todo={currentTodo} key={i} />;
-            //return
-            //(
-            // <tr key={i}>
-            //     <td>{todo_description}</td>
-            //     <td>{todo_responsible}</td>
-            //     <td>{todo_priority}</td>
-            //     <td>{todo_completed}</td>
-            //     <td>
-            //         <Link to={"/edit/" + _id}>Edit</Link>
-            //     </td>
-            //     <td>
-            //         <button size="sm" variant="danger" onClick={this.delRecord}>Delete</button>
-            //     </td>
-            // </tr>
-            //)
+        return this.state.todos.map(function (currentTodo, i) {
+            return <TodoTableRow todo={currentTodo} key={i} />;
         })
     }
 
@@ -87,9 +38,7 @@ export default class TodosList extends React.Component {
                             <th>Responsible</th>
                             <th>Priority</th>
                             <th>Action</th>
-                            <th>
-                                Delete
-                            </th>
+                            <th>Delete</th>
                         </tr>
                     </thead>
                     <tbody>
